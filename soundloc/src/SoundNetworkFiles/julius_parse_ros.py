013c653bb247205ff98cc6b08c1dc84ca97903b2
Victor Yap <victor@vicyap-VirtualBox.(none)>
2013-12-15 13:30:39 -0600

    added MultiSpeechRecog.n and julius_parse_ros.py which reads in stdin from julius and parses out useful information. Then it publishes /julius/parse/sentence as a single string message

---
+++ soundloc/src/SoundNetworkFiles/julius_parse_ros.py
@@ -0,0 +1,31 @@
+import sys
+import roslib
+import rospy
+from std_msgs.msg import String
+
+
+def talker():
+    pub_sid = rospy.Publisher('julius/parse/source_id', String)
+    pub_sentence = rospy.Publisher('julius/parse/sentence', String)
+    rospy.init_node('julius_parse_ros')
+    while not rospy.is_shutdown():
+        source = sys.stdin.readline().strip('\n')
+        if source.startswith('source_id'):
+            pub_sid.publish(String(source))
+            params = source.split(',')
+            sentence = sys.stdin.readline().strip('\n')
+            while not sentence.startswith('sentence1'):
+                sentence = sys.stdin.readline().strip('\n')
+                if sentence.startswith('sentence1:'):
+                    message = "%s; %s; %s; %s" % (params[0][12:], sentence[15:-5], params[1][11:], rospy.get_time())
+                    pub_sentence.publish(String(message))
+                    rospy.loginfo(sentence)
+        
+
+if __name__ == '__main__':
+    try:
+        while 1:
+            talker()
+    except KeyboardInterrupt:
+        sys.exit(1)
+	
