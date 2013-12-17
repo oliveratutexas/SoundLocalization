289a3be02d936f43c89ef7374638443f726646bf
Oliver <oliver@azul.me>
2013-12-16 19:47:24 -0600

    Added extra code for rock paper scissors game

--- scripts/voice_rps.py
+++ scripts/voice_rps.py
@@ -10,37 +10,43 @@ is_playing = False
 time = 0.0
 left = None
 right = None
+answers = ['paper', 'scissors', 'rock']
 
 def callback(speech_data):
-    rospy.loginfo(speech_data.sentence)
-    global is_playing, time, left, right
+    #rospy.loginfo(speech_data.sentence)
+    global is_playing, time, left, right, answers
     if not is_playing and speech_data.sentence.startswith('lets-play'):
         is_playing = True
-        left = None
-        right = None
-        rospy.sleep(1.0)
+        left = ''
+        right = ''
         rospy.loginfo("Get Ready")
         rospy.sleep(1.0)
         rospy.loginfo("Get Set")
         rospy.sleep(1.0)
         rospy.loginfo("Speak!")
         time = speech_data.rostime
-	rospy.loginfo(time)
-    if is_playing and rospy.get_time() - time > 2:
-        if speech_data.azimuth < -10 and right is None:
-            right = speech_data.sentence
-        elif speech_data.azimuth > 10 and left is None:
-            left = speech_data.sentence
-        if left is not None and right is not None:
+	#rospy.loginfo(time)
+    word = speech_data.sentence.split(' ')[0]
+    if is_playing and word in answers:
+        if speech_data.azimuth < -10 and right is '':
+            right = word
+        elif speech_data.azimuth > 10 and left is '':
+            left = word
+        if left is not '' and right is not '':
             rospy.loginfo("The game is successful")
+	    rospy.loginfo("right: %s, left: %s" % (right, left))
+	    if left == right:
+		rospy.loginfo("TIE")
+	    elif ((left is 'paper' and right is 'rock') or (left is 'rock' and right is 'scissors') or (left is 'scissors' and right is 'paper')):
+		rospy.loginfo("<-- WINNER")
+	    else:
+		rospy.loginfo("WINNER -->")
             is_playing = False
-    if rospy.get_time() - time > 2:
-	rospy.loginfo("The current game has timed out")
-	is_playing = False
-	left = None
-	right = None
-
-        
+#    if rospy.get_time() - time > 2000:
+#	rospy.loginfo("The current game has timed out")
+#	is_playing = False
+#	left = None
+#	right = None       
 
 if __name__ == '__main__':
     try:
@@ -48,7 +54,6 @@ if __name__ == '__main__':
             rospy.init_node('voice_rps', anonymous=True)
             rospy.Subscriber("/Speech", Speech, callback)
             rospy.spin()
-            
     except KeyboardInterrupt:
         sys.exit(1)
 	
